6dc16c63497a052aff10ba2e7eac8cf2c27e2951
André de Souza <andreoew@hotmail.com>
2022-08-26 15:32:13 -0300

    udate cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,9 @@ import linksRouter from './routes/links';
 import cors from 'cors';
 const app = express();
 
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 app.use(express.json());
+
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
