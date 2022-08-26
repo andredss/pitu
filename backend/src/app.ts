ec5cbc9a9ff08103300fc17e3722c3c6b0a2226c
André de Souza <andreoew@hotmail.com>
2022-08-26 15:20:44 -0300

    update cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,8 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(linksRouter);
 
 export default app;
