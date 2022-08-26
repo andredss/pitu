5e3aed6e02d89ccfb31bb27ee55a877f1188cde8
André de Souza <andreoew@hotmail.com>
2022-08-26 15:30:51 -0300

    update cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import linksRouter from './routes/links';
 import cors from 'cors';
-
 const app = express();
+
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(express.json());
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
