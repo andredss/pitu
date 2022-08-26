c2cd5f6a323635162c62d629fbc00b2cf4be1de5
André de Souza <andreoew@hotmail.com>
2022-08-26 14:41:43 -0300

    update env

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,13 @@
 import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+console.log(process.env.NODE_ENV)
+
 
 const sequelize = new Sequelize(`${process.env.CONNECTION_STRING}`);
+
+  
 
 export default sequelize;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,3 +6,8 @@ console.log('Database running at 3306');
 
 app.listen(process.env.PORT);
 console.log(`Server running at ${process.env.PORT}`);
+
+// app.listen(process.env.PORT || 3001, () => {
+//   // console.log(`Server running at 3001`);
+//   console.log(`Server running at ${process.env.PORT}`);
+// });
